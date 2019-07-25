6501765b5389ea06b389372c761afb0d16083a5a
Desmond Ngwuta <desmondmuqsit@yahoo.com>
2019-07-25 22:06:06 +0100

    update() and insert()

--- app/src/main/java/com/chydee/notekeeper/NoteDao.java
+++ app/src/main/java/com/chydee/notekeeper/NoteDao.java
@@ -1,7 +1,13 @@
 package com.chydee.notekeeper;
 
 import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Update;
 
 @Dao
 public interface NoteDao {
+    @Insert
+    void insert(Note note);
+    @Update
+    void update(Note note);
 }
