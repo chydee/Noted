c39ff4fe3e50a98e0b7c4b6cbbc4eaf483dfcb84
Desmond Ngwuta <desmondmuqsit@yahoo.com>
2019-07-26 21:24:06 +0100

    update: RoomDatabase

--- app/src/main/java/com/chydee/notekeeper/NoteDatabase.java
+++ app/src/main/java/com/chydee/notekeeper/NoteDatabase.java
@@ -1,6 +1,8 @@
 package com.chydee.notekeeper;
 
+import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
+@Database(entities = {Note.class}, version = 1)
 public abstract class NoteDatabase extends RoomDatabase {
 }
