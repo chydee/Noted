3595c8b392398532f01decd94ba1a68a22c606ba
Desmond Ngwuta <desmondmuqsit@yahoo.com>
2019-07-27 13:21:13 +0100

    variables: NoteDao and LiveData declared

--- app/src/main/java/com/chydee/notekeeper/NoteRepository.java
+++ app/src/main/java/com/chydee/notekeeper/NoteRepository.java
@@ -1,4 +1,12 @@
 package com.chydee.notekeeper;
 
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
 public class NoteRepository {
+
+    private NoteDao noteDao;
+    private LiveData<List<Note>> allNotes;
+
 }
