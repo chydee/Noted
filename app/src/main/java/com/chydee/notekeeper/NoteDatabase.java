f7464297a233098d73c303b1a985caba43d50061
Desmond Ngwuta <desmondmuqsit@yahoo.com>
2019-07-27 13:05:57 +0100

    update: RoomDatabase

--- app/src/main/java/com/chydee/notekeeper/NoteDatabase.java
+++ app/src/main/java/com/chydee/notekeeper/NoteDatabase.java
@@ -1,6 +1,9 @@
 package com.chydee.notekeeper;
 
+import android.content.Context;
+
 import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
 @Database(entities = {Note.class}, version = 1)
@@ -9,4 +12,15 @@ public abstract class NoteDatabase extends RoomDatabase {
     private static NoteDatabase instance;
 
     public abstract NoteDao noteDao();
+
+    public static synchronized NoteDatabase getInstance(Context context){
+        if (instance == null){
+            instance = Room.databaseBuilder(context.getApplicationContext(),
+                    NoteDatabase.class, "note_database")
+                    .fallbackToDestructiveMigration()
+                    .build();
+        }
+
+        return instance;
+    }
 }
