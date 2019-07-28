47341261b130670f5cbfc943d344d2a3abc46c61
Desmond Ngwuta <desmondmuqsit@yahoo.com>
2019-07-28 06:19:38 +0100

    NoteViewModel created and updated

---
+++ app/src/main/java/com/chydee/notekeeper/NoteViewModel.java
@@ -0,0 +1,37 @@
+package com.chydee.notekeeper;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+public class NoteViewModel extends AndroidViewModel {
+
+    private NoteRepository mRepository;
+    private LiveData<List<Note>> allNotes;
+
+    public NoteViewModel(@NonNull Application application) {
+        super(application);
+        mRepository = new NoteRepository(application);
+        allNotes = mRepository.getAllNotes();
+    }
+
+    public void insert(Note note){
+        mRepository.insert(note);
+    }
+
+    public void update(Note note){
+        mRepository.update(note);
+    }
+
+    public void delete(Note note){
+        mRepository.delete(note);
+    }
+
+    public void deleteAllNotes(){
+        mRepository.deleteAll();
+    }
+}
