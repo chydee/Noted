59fd00af0878e5dfe289f4926e0216649b90f1c8
Desmond Ngwuta <desmondmuqsit@yahoo.com>
2019-07-28 06:31:32 +0100

    Retrieve LiveData in MainActivity.class

--- app/src/main/java/com/chydee/notekeeper/MainActivity.java
+++ app/src/main/java/com/chydee/notekeeper/MainActivity.java
@@ -1,14 +1,30 @@
 package com.chydee.notekeeper;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private NoteViewModel mNoteViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mNoteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
+        mNoteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
+            @Override
+            public void onChanged(List<Note> notes) {
+                //Update RecyclerView
+                Toast.makeText(MainActivity.this, "onChanged", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
