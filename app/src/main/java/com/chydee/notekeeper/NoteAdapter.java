e6a5c12fb263d4a27de45930bdd75783053dd7c9
Desmond Ngwuta <desmondmuqsit@yahoo.com>
2019-07-29 23:06:14 +0100

    update: NoteAdapter.java description: create constuctor matching super and implemented the required methods

--- app/src/main/java/com/chydee/notekeeper/NoteAdapter.java
+++ app/src/main/java/com/chydee/notekeeper/NoteAdapter.java
@@ -1,12 +1,37 @@
 package com.chydee.notekeeper;
 
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class NoteAdapter extends RecyclerView.Adapter {
+import java.util.ArrayList;
+import java.util.List;
+
+public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteHolder> {
+
+    private List<Note> mNotes = new ArrayList<>();
+
+    @NonNull
+    @Override
+    public NoteHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.note_item, parent, false);
+        return new NoteHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NoteHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 
     //Create a view holder class
     //This will hold the views in the recyclerView item
