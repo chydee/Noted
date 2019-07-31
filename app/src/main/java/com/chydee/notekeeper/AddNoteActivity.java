7ca4182f8975f8cb31b5ba443a82252163e9fa97
Desmond Ngwuta <desmondmuqsit@yahoo.com>
2019-07-31 13:26:22 +0100

    update: AddNoteActivity.java description: wired up the activity with the layout, set a reference to the widgets and also the min and max values of the number picker

--- app/src/main/java/com/chydee/notekeeper/AddNoteActivity.java
+++ app/src/main/java/com/chydee/notekeeper/AddNoteActivity.java
@@ -1,14 +1,27 @@
 package com.chydee.notekeeper;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.NumberPicker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AddNoteActivity extends AppCompatActivity {
 
+    private EditText mEditTextTitle;
+    private EditText mEditTextDescription;
+    private NumberPicker mNumberPickerPriority;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_note);
+
+        mEditTextTitle = findViewById(R.id.edit_text_title);
+        mEditTextDescription = findViewById(R.id.edit_text_description);
+        mNumberPickerPriority = findViewById(R.id.number_picker_priority);
+
+        mNumberPickerPriority.setMinValue(1);
+        mNumberPickerPriority.setMaxValue(10);
     }
 }
