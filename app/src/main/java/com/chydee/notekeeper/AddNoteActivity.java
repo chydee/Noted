a06a0f8139b70a96270e221f6d9b41aea5e1e3ab
Desmond Ngwuta <desmondmuqsit@yahoo.com>
2019-07-31 12:36:53 +0100

    update: new activities added && build.gradle updated description: updated the lifecycle_version and created a new activity for adding notes

---
+++ app/src/main/java/com/chydee/notekeeper/AddNoteActivity.java
@@ -0,0 +1,14 @@
+package com.chydee.notekeeper;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddNoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_note);
+    }
+}
