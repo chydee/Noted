8b4082c184ab7f0f801abeab1c27eade1392f7de
Desmond Ngwuta <desmondmuqsit@yahoo.com>
2019-07-31 09:47:19 +0100

    update: NoteAdapter.java description: All that is needed here has been done

--- app/src/main/java/com/chydee/notekeeper/MainActivity.java
+++ app/src/main/java/com/chydee/notekeeper/MainActivity.java
@@ -1,11 +1,12 @@
 package com.chydee.notekeeper;
 
 import android.os.Bundle;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
@@ -18,12 +19,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        recyclerView.setHasFixedSize(true);// you should set this as true if you know that the recycler view size wont change
+        //As this makes it more efficient
+
+        final NoteAdapter mAdapter = new NoteAdapter();
+        recyclerView.setAdapter(mAdapter);
+
         mNoteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
         mNoteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
             @Override
             public void onChanged(List<Note> notes) {
                 //Update RecyclerView
-                Toast.makeText(MainActivity.this, "onChanged", Toast.LENGTH_SHORT).show();
+                mAdapter.setNotes(notes);
             }
         });
     }
