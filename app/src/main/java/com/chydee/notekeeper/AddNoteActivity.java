f777f23ff8328c624c971ef5f111b164e9886cac
Desmond Ngwuta <desmondmuqsit@yahoo.com>
2019-07-31 13:37:52 +0100

    update: AddNoteActivity.java description: onCreateOptionsMenu called

--- app/src/main/java/com/chydee/notekeeper/AddNoteActivity.java
+++ app/src/main/java/com/chydee/notekeeper/AddNoteActivity.java
@@ -1,6 +1,8 @@
 package com.chydee.notekeeper;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.widget.EditText;
 import android.widget.NumberPicker;
 
@@ -23,5 +25,21 @@ public class AddNoteActivity extends AppCompatActivity {
 
         mNumberPickerPriority.setMinValue(1);
         mNumberPickerPriority.setMaxValue(10);
+
+        //In order to get the little 'x' in the top left corner of the action bar
+        //i.e the close o exit activity button,
+        // We call :
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close);
+        setTitle("Add Note");// This will just display the "Add Note" text in the action bar
+    }
+
+    //So to comfirm the inputs when we click the save icon in the top menu right corner of the action bar
+    //First of all we have to get this icon there. We do this by overwriting :
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.add_note_menu, menu);
+        return true;// Which means that we want to display the menu
     }
 }
