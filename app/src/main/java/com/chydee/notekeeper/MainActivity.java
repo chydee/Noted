6bebb408be62e06fc413aa4b2f9d3eb598d9234b
Desmond Ngwuta <desmondmuqsit@yahoo.com>
2019-08-01 23:14:01 +0100

    update: MainActivity.java description: Wired up the FloatingActionButton and setOnClickListener() with startActivityForResult set.

--- app/src/main/java/com/chydee/notekeeper/MainActivity.java
+++ app/src/main/java/com/chydee/notekeeper/MainActivity.java
@@ -1,6 +1,8 @@
 package com.chydee.notekeeper;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
@@ -8,9 +10,13 @@ import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     private NoteViewModel mNoteViewModel;
 
@@ -18,6 +24,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton buttonAddNote = findViewById(R.id.button_add_note);
+        buttonAddNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AddNoteActivity.class);
+                startActivityForResult(intent,);
+            }
+        });
 
         RecyclerView recyclerView = findViewById(R.id.recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
