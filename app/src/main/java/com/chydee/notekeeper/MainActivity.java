d29761c6b04da6c1cbc41d243dcc4f07a2522606
Desmond Ngwuta <desmondmuqsit@yahoo.com>
2019-08-03 23:45:13 +0100

    update: MainActivity description: onCreateOptionsMenu

--- app/src/main/java/com/chydee/notekeeper/MainActivity.java
+++ app/src/main/java/com/chydee/notekeeper/MainActivity.java
@@ -2,6 +2,8 @@ package com.chydee.notekeeper;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Toast;
 
@@ -90,5 +92,12 @@ public class MainActivity extends AppCompatActivity {
         } else {
             Toast.makeText(this, "Note discarded", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu, menu);
+        return true;
     }
 }
