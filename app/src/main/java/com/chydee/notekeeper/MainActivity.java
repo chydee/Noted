80b957242084f9b32d2256ce3b098a628ef86f58
Desmond Ngwuta <desmondmuqsit@yahoo.com>
2019-08-04 22:55:00 +0100

    startIntentForResult

--- app/src/main/java/com/chydee/notekeeper/MainActivity.java
+++ app/src/main/java/com/chydee/notekeeper/MainActivity.java
@@ -80,9 +80,12 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemClick(Note note) {
                 Intent intent = new Intent(MainActivity.this, AddEditNoteActivity.class);
+                intent.putExtra(AddEditNoteActivity.EXTRA_ID, note.getId());
                 intent.putExtra(AddEditNoteActivity.EXTRA_TITLE, note.getTitle());
                 intent.putExtra(AddEditNoteActivity.EXTRA_DESCRIPTION, note.getDescription());
                 intent.putExtra(AddEditNoteActivity.EXTRA_PRIORITY, note.getPriority());
+
+                startActivityForResult(intent, EDIT_NOTE_REQUEST);
             }
         });
     }
