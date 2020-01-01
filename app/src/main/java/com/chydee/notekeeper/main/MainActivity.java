42f403c72edbc5d7aadc6cd40653d39220fa2d06
Chydee <desmondmuqsit@yahoo.com>
2020-01-01 02:52:41 +0100

    Separated classes into smaller packages Because: - It makes the code directory cleaner - It is good for Modularizing
    This commit will separate classes into packages
    
    Author: Chydee

--- app/src/main/java/com/chydee/notekeeper/main/MainActivity.java
+++ app/src/main/java/com/chydee/notekeeper/main/MainActivity.java
@@ -1,4 +1,4 @@
-package com.chydee.notekeeper;
+package com.chydee.notekeeper.main;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -17,6 +17,11 @@ import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.chydee.notekeeper.AddEditNoteActivity;
+import com.chydee.notekeeper.Note;
+import com.chydee.notekeeper.NoteAdapter;
+import com.chydee.notekeeper.NoteViewModel;
+import com.chydee.notekeeper.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.List;
