1d03f31ef7f144e5b6f522bd16cb01260e1ae247
Chydee <desmondmuqsit@yahoo.com>
2020-02-02 22:15:21 +0100

    updated

--- app/src/main/java/com/chydee/notekeeper/database/Note.kt
+++ app/src/main/java/com/chydee/notekeeper/database/Note.kt
@@ -4,8 +4,8 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "note_keeper_table")
-data class NoteDatabase(
+@Entity(tableName = "note_table")
+data class Note(
         @PrimaryKey(autoGenerate = true)
         var noteId: Long,
 
