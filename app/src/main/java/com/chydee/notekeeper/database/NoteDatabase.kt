63e68ebf9f2171c59dfa09f167c57052fd5f2f8b
Chydee <desmondmuqsit@yahoo.com>
2020-02-02 21:08:26 +0100

    updated: Added constructor to the NoteDatabase

--- app/src/main/java/com/chydee/notekeeper/database/NoteDatabase.kt
+++ app/src/main/java/com/chydee/notekeeper/database/NoteDatabase.kt
@@ -1,3 +1,19 @@
 package com.chydee.notekeeper.database
 
-class NoteDatabase
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "note_keeper_table")
+data class NoteDatabase(
+        @PrimaryKey(autoGenerate = true)
+        var noteId: Long,
+
+        @ColumnInfo(name = "note_title")
+        var noteTitle: String,
+
+        @ColumnInfo(name = "note_detail")
+        var noteDetail: String
+
+)
+
