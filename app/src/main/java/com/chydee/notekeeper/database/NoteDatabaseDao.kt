f01b59173c9c66e9fb0b3fbe4b84f3c09b866e66
Chydee <desmondmuqsit@yahoo.com>
2020-02-02 21:22:35 +0100

    updated

--- app/src/main/java/com/chydee/notekeeper/database/NoteDatabaseDao.kt
+++ app/src/main/java/com/chydee/notekeeper/database/NoteDatabaseDao.kt
@@ -1,8 +1,35 @@
 package com.chydee.notekeeper.database
 
-import androidx.room.Dao
+import androidx.lifecycle.LiveData
+import androidx.room.*
 
 
 //Defines the method for using the Note class with room
 @Dao
-interface NoteDatabaseDao
+interface NoteDatabaseDao {
+
+    /*
+    * This method inserts the note into the database
+    */
+    @Insert
+    fun insert(note: Note)
+
+    /*
+    * This method updates a single note from the note database
+     */
+    @Update
+    fun update(note: Note)
+
+    @Delete
+
+    /**
+     * Deletes all values from the table.
+     *
+     * This does not delete the table, only its contents.
+     */
+    @Query("DELETE FROM note_table")
+    fun clear()
+
+    @Query("SELECT * FROM note_table ORDER BY noteId DESC")
+    fun getAllNotes(): LiveData<List<Note>>
+}
