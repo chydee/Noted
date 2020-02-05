f9dab9534ec6766fb8b4e66e53d58eb2376b2885
Chydee <desmondmuqsit@yahoo.com>
2020-02-05 20:35:49 +0100

    re-wrote another instrumentation test Hope it works

---
+++ app/src/androidTest/java/com/chydee/notekeeper/database/NoteDatabaseTest.kt
@@ -0,0 +1,42 @@
+package com.chydee.notekeeper.database
+
+import androidx.room.Room
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.After
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.io.IOException
+
+@RunWith(AndroidJUnit4::class)
+class NoteDatabaseTest {
+
+    private lateinit var noteDatabaseDao: NoteDatabaseDao
+    private lateinit var noteDb: NoteDatabase
+
+    @Before
+    fun setUp() {
+        val context = InstrumentationRegistry.getInstrumentation().targetContext
+        noteDb = Room.inMemoryDatabaseBuilder(context, NoteDatabase::class.java)
+                .allowMainThreadQueries()
+                .build()
+        noteDatabaseDao = noteDb.noteDatabaseDao
+    }
+
+    @After
+    @Throws(IOException::class)
+    fun tearDown() {
+        noteDb.close()
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun insertAndGetNote() {
+        val note = Note(noteId = 1, noteTitle = "Test Implementation", noteDetail = "Instrumentation Test for the database")
+        noteDatabaseDao.insert(note)
+        val thisNote = noteDatabaseDao.getThisNote()
+        Assert.assertEquals(thisNote?.noteId, 1)
+    }
+}
