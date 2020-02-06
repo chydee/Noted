5c66675c06151726fa3db61899b03bc248a9f5dc
Chydee <desmondmuqsit@yahoo.com>
2020-02-06 03:49:16 +0100

    updated but it still ain't working

--- app/src/main/java/com/chydee/notekeeper/main/MainActivity.java
+++ app/src/main/java/com/chydee/notekeeper/main/MainActivity.java
@@ -1,7 +1,6 @@
 package com.chydee.notekeeper.main;
 
 import android.os.Bundle;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,9 +10,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
-
-        Toast.makeText(this, "We are here", Toast.LENGTH_SHORT).show();
     }
 }
