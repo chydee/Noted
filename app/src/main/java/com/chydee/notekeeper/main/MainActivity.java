35518ebd68edb21992d4a9d7442d9f22b33901f7
Chydee <desmondmuqsit@yahoo.com>
2020-03-09 23:10:14 +0100

    updated

--- app/src/main/java/com/chydee/notekeeper/main/MainActivity.java
+++ app/src/main/java/com/chydee/notekeeper/main/MainActivity.java
@@ -11,7 +11,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        setSupportActionBar();
     }
 }
