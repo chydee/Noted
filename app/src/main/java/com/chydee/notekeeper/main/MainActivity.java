5ee7ddd9047fc7a30bc9185c1bd6e857cfc873bf
Chydee <desmondmuqsit@yahoo.com>
2020-03-10 12:21:35 +0100

    set up the binder

--- app/src/main/java/com/chydee/notekeeper/main/MainActivity.java
+++ app/src/main/java/com/chydee/notekeeper/main/MainActivity.java
@@ -4,12 +4,14 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.chydee.notekeeper.R;
+import com.chydee.notekeeper.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
     }
 }
