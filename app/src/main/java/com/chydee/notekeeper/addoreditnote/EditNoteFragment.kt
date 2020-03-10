5b92b5a0a9578acdeff397068ddee7dfa5fc8bc0
Chydee <desmondmuqsit@yahoo.com>
2020-03-10 15:18:10 +0100

    updated

--- app/src/main/java/com/chydee/notekeeper/addoreditnote/EditNoteFragment.kt
+++ app/src/main/java/com/chydee/notekeeper/addoreditnote/EditNoteFragment.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import com.chydee.notekeeper.R
+import com.chydee.notekeeper.databinding.EditNoteFragmentBinding
 
 
 class EditNoteFragment : Fragment() {
@@ -19,7 +21,9 @@ class EditNoteFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.edit_note_fragment, container, false)
+        val binding: EditNoteFragmentBinding = DataBindingUtil.inflate(inflater, R.layout.edit_note_fragment, container, false)
+        setHasOptionsMenu(true)
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
--- app/src/main/java/com/chydee/notekeeper/main/MainActivity.java
+++ app/src/main/java/com/chydee/notekeeper/main/MainActivity.java
@@ -1,20 +1,44 @@
 package com.chydee.notekeeper.main;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
+import androidx.navigation.Navigation;
 
+import com.chydee.notekeeper.R;
 import com.chydee.notekeeper.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String TAG = MainActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        final ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
         setSupportActionBar(binding.mainToolbar);
+        NavController navController = Navigation.findNavController(MainActivity.this, R.id.nav_host_fragment);
+
+        navController.addOnDestinationChangedListener(new NavController.OnDestinationChangedListener() {
+            @Override
+            public void onDestinationChanged(@NonNull NavController controller, @NonNull NavDestination destination, @Nullable Bundle arguments) {
+                if (destination.getLabel() == "home_fragment")
+                    setSupportActionBar(binding.mainToolbar);
+                else if (destination.getLabel() == "edit_note_fragment")
+                    setSupportActionBar(binding.editNoteToolbar);
+                else
+                    Log.e(TAG, "onDestinationChanged: " + destination.getLabel());
+            }
+        });
     }
+
+
 }
