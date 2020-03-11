6830ff3409875c1dd0b8af41cb1f96493b543562
Chydee <desmondmuqsit@yahoo.com>
2020-03-11 13:26:52 +0100

    working but need =s some fixing up

--- app/src/main/java/com/chydee/notekeeper/addoreditnote/EditNoteFragment.kt
+++ app/src/main/java/com/chydee/notekeeper/addoreditnote/EditNoteFragment.kt
@@ -13,8 +13,8 @@ import com.google.android.material.bottomsheet.BottomSheetBehavior
 
 class EditNoteFragment : Fragment() {
 
-    private val sheetBehavior: BottomSheetBehavior<*>? = null
-    private val options_bottom_sheet: LinearLayout? = null
+    private var sheetBehavior: BottomSheetBehavior<View>? = null
+    private var options_bottom_sheet: LinearLayout? = null
 
     private lateinit var viewModel: EditNoteViewModel
 
@@ -22,6 +22,17 @@ class EditNoteFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         val binding: EditNoteFragmentBinding = DataBindingUtil.inflate(inflater, R.layout.edit_note_fragment, container, false)
         setHasOptionsMenu(true)
+        options_bottom_sheet = binding.root.findViewById(R.id.options_bottom_sheet_layout)
+        sheetBehavior = BottomSheetBehavior.from(options_bottom_sheet!!)
+        binding.bottomSheet.setOnClickListener { v ->
+            (
+                    if (sheetBehavior!!.state != BottomSheetBehavior.STATE_EXPANDED) {
+                        sheetBehavior!!.setState(BottomSheetBehavior.STATE_EXPANDED)
+                    } else {
+                        sheetBehavior!!.setState(BottomSheetBehavior.STATE_COLLAPSED)
+                    }
+                    )
+        }
         return binding.root
     }
 
