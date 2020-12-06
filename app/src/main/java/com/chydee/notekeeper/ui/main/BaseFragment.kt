9d1f26593d31e549ff2e67d1a8c9d6214d9a84c4
Ngwuta Desmond <desmondmuqsit@yahoo.com>
2020-12-06 02:47:06 +0100

    update: setup mediaRecorder

--- app/src/main/java/com/chydee/notekeeper/ui/main/BaseFragment.kt
+++ app/src/main/java/com/chydee/notekeeper/ui/main/BaseFragment.kt
@@ -1,9 +1,11 @@
 package com.chydee.notekeeper.ui.main
 
+import android.content.Context
 import androidx.fragment.app.Fragment
 import com.chydee.notekeeper.R
 import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.snackbar.Snackbar
+import java.io.File
 
 abstract class BaseFragment : Fragment() {
 
@@ -32,6 +34,14 @@ abstract class BaseFragment : Fragment() {
                     }
                     .show()
         }
+    }
 
+    fun getOutputDirectory(context: Context): File {
+        val appContext = context.applicationContext
+        val mediaDir = context.externalMediaDirs.firstOrNull()?.let {
+            File(it, appContext.resources.getString(R.string.app_name)).apply { mkdirs() }
+        }
+        return if (mediaDir != null && mediaDir.exists())
+            mediaDir else appContext.filesDir
     }
 }
--- app/src/main/java/com/chydee/notekeeper/ui/voice/VoiceNotesFragment.kt
+++ app/src/main/java/com/chydee/notekeeper/ui/voice/VoiceNotesFragment.kt
@@ -1,18 +1,25 @@
 package com.chydee.notekeeper.ui.voice
 
+import android.annotation.SuppressLint
+import android.annotation.TargetApi
 import android.content.Context
+import android.media.MediaRecorder
+import android.os.Build
 import android.os.Bundle
 import android.os.Environment
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
+import com.chydee.notekeeper.R
 import com.chydee.notekeeper.databinding.VoiceNotesFragmentBinding
 import com.chydee.notekeeper.ui.main.BaseFragment
 import java.io.File
-import java.io.FileNotFoundException
-import java.io.FileOutputStream
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
 class VoiceNotesFragment : BaseFragment(), RecordNoteBottomSheet.OnClickListener {
 
@@ -20,36 +27,80 @@ class VoiceNotesFragment : BaseFragment(), RecordNoteBottomSheet.OnClickListener
 
     private lateinit var binding: VoiceNotesFragmentBinding
 
+    private var output: String? = null
+    private var mediaRecorder: MediaRecorder? = null
+    private var state: Boolean = false
+    private var recordingStopped: Boolean = false
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         binding = VoiceNotesFragmentBinding.inflate(inflater)
         return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(this).get(VoiceNotesViewModel::class.java)
         binding.recordNewVoiceNote.setOnClickListener {
             RecordNoteBottomSheet.instanceOfThis(this).show(childFragmentManager, "RecordNotes")
         }
+        output = "${getOutputDirectory(requireContext())}/${
+            SimpleDateFormat(
+                    getString(R.string.date_time_string_format), Locale.ROOT
+            ).format(System.currentTimeMillis())
+        }.mp3"
+        mediaRecorder = MediaRecorder()
+        mediaRecorder?.apply {
+            setAudioSource(MediaRecorder.AudioSource.MIC)
+            setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
+            setAudioEncoder(MediaRecorder.AudioEncoder.AAC)
+            setOutputFile(output)
+        }
     }
 
 
-    private fun saveNote(fileName: String) {
-        val f: File = File("/somedir")
-        // create a File object for the parent directory
-        val notedAudiosDir = File("/voice_notes/")
-        // have the object build the directory structure, if needed.
-        notedAudiosDir.mkdirs()
-        // create a File object for the output file
-        val outputFile = File(notedAudiosDir, fileName)
-        // now attach the OutputStream to the file object, instead of a String representation
+    private fun startRecording() {
         try {
-            val fos = FileOutputStream(outputFile)
-        } catch (e: FileNotFoundException) {
+            mediaRecorder?.prepare()
+            mediaRecorder?.start()
+            state = true
+            Toast.makeText(context, "Recording started!", Toast.LENGTH_SHORT).show()
+        } catch (e: IllegalStateException) {
+            e.printStackTrace()
+        } catch (e: IOException) {
             e.printStackTrace()
         }
+    }
 
+    private fun stopRecording() {
+        if (state) {
+            mediaRecorder?.stop()
+            mediaRecorder?.release()
+            state = false
+        } else {
+            Toast.makeText(context, "You are not recording right now!", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.N)
+    private fun pauseRecording() {
+        if (state) {
+            if (!recordingStopped) {
+                Toast.makeText(context, "Stopped!", Toast.LENGTH_SHORT).show()
+                mediaRecorder?.pause()
+                recordingStopped = true
+            } else {
+                resumeRecording()
+            }
+        }
+    }
+
+    @SuppressLint("RestrictedApi", "SetTextI18n")
+    @TargetApi(Build.VERSION_CODES.N)
+    private fun resumeRecording() {
+        Toast.makeText(context, "Resume!", Toast.LENGTH_SHORT).show()
+        mediaRecorder?.resume()
+        recordingStopped = false
     }
 
     fun getAppSpecificAudios(context: Context, albumName: String): File? {
@@ -65,14 +116,17 @@ class VoiceNotesFragment : BaseFragment(), RecordNoteBottomSheet.OnClickListener
 
     override fun onStartRecording() {
         //Start Recording Note
+        startRecording()
     }
 
     override fun onPauseRecording() {
         //Pause Recording  and Release Media
+        pauseRecording()
     }
 
     override fun onStopRecording() {
         //Stop Recording and Release Media
+        stopRecording()
     }
 
 }
